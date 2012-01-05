55981f95f3a74098905af6d228f99954b0c46c9a
nergal <nergal@nergal.org.ua>
2012-01-05 13:38:38 +0300

    Апдейт файлов из живого проекта

--- classes/controller/resizer.php
+++ classes/controller/resizer.php
@@ -8,41 +8,42 @@ class Controller_Resizer extends Controller {
 	public function before()
 	{
 	    $this->cache_dir = Kohana::$cache_dir.DIRECTORY_SEPARATOR.'resizer';
+	    $this->cache_dir = DOCROOT.'thumbnails/';
 	    if ( ! file_exists($this->cache_dir)) {
-		mkdir($this->cache_dir, 0700, TRUE);
+			mkdir($this->cache_dir, 0755, TRUE);
 	    }
 	}
-	
-	private function _get_cache_path($source_path, $type, $width, $height, $filename)
+
+	private function _get_cache_path($source_path, $type, $width, $height, $filename, $ext)
 	{
 		$path = array($this->cache_dir);
 		$path[] = $source_path;
 		$path[] = "{$type}_{$width}x{$height}";
 		$path = implode(DIRECTORY_SEPARATOR, $path);
-		
+
 		if ( ! file_exists($path)) {
-			mkdir($path, 0700, TRUE);
+			mkdir($path, 0755, TRUE);
 		}
-		
-		$file = $path.DIRECTORY_SEPARATOR.$filename.'.jpeg';
+
+		$file = $path.DIRECTORY_SEPARATOR.$filename.'.'.$ext;
 		return $file;
 	}
 
 	public function action_index($source_path, $type, $width, $height, $filename, $ext)
 	{
-		$path = array(DOCROOT.'uploads');
+		$path = array(DOCROOT.'images');
 		$path[] = $source_path;
 		$path[] = $filename.'.'.$ext;
 		$path = implode(DIRECTORY_SEPARATOR, $path);
-		
-		$config = Kohana::config('resizer');
-		
+
+		$config = Kohana::$config->load('resizer');
+
 		$allow_resize = FALSE;
 		foreach ($config->get('allowed_sizes') as $size) {
 			if ( ! is_array($size)) {
 				$size = explode('x', $size);
 			}
-			
+
 			if (intVal($size[0]) == $width) {
 				if (intVal($size[1]) == $height) {
 					$allow_resize = TRUE;
@@ -50,36 +51,58 @@ class Controller_Resizer extends Controller {
 				}
 			}
 		}
-		
-		if (realpath($path) AND $allow_resize === TRUE) {
-			$cache_file = $this->_get_cache_path($source_path, $type, $width, $height, $filename);
-			
+
+		$path = realpath($path);
+
+		if ( ! $path) {
+			$path = array(DOCROOT.'uploads');
+			$path[] = $source_path;
+			$path[] = $filename.'.'.$ext;
+			$path = implode(DIRECTORY_SEPARATOR, $path);
+
+			$path = realpath($path);
+		}
+
+		if ( ! $path) {
+			$path = array(DOCROOT.'thumbnails');
+			$path[] = $source_path;
+			$path[] = $filename.'.'.$ext;
+			$path = implode(DIRECTORY_SEPARATOR, $path);
+
+			$path = realpath($path);
+		}
+
+		if ($path AND $allow_resize === TRUE) {
+			$cache_file = $this->_get_cache_path($source_path, $type, $width, $height, $filename, $ext);
+
 			if ( ! file_exists($cache_file)) {
 				$image = Image::factory($path, 'gd');
 				switch ($type) {
 					case 'res':
-						$image->resize($width, $height);
+						$image->resize($width, $height, Image::NONE);
+						break;
+					case 'cropg':
+						$image->resize($width, $height, Image::INVERSE);
 						break;
 					case 'cropr':
-						$orientation = ($width > $height) ? Image::WIDTH : Image::HEIGHT;
-						$image->resize($width, $height, $orientation);
+						$image->resize($width, $height, Image::INVERSE);
 					case 'crop':
 						$image->crop($width, $height);
 						break;
 					default:
 						throw new Exception;
 				}
-				$data = $image->render();
+				$data = $image->render($ext);
 				$image->save($cache_file);
 			} else {
 				$data = file_get_contents($cache_file);
 			}
-			$this->response->headers('Content-Type', 'image/jpeg');
+			$this->response->headers('Content-Type', 'image/'.$ext);
 		} else {
 			$this->response->headers('Content-Type', 'image/png');
 			$data = base64_decode($this->empty_png);
 		}
-		
+
 		$this->response->body($data);
 	}
 
--- init.php
+++ init.php
@@ -2,17 +2,17 @@
 
 Route::set(
 	'resizer',
-	'thumb(/<path>)/<type>_<x>x<y>/<name>.<ext>',
+	'thumbnails(/<path>)/<type>_<x>x<y>/<name>.<ext>',
 	array(
 		'path' => '.*',
-		'type' => '(res|crop|cropr)',
+		'type' => '(res|crop|cropr|cropg)',
 		'x'    => '[\d]{1,3}',
 		'y'    => '[\d]{1,3}',
-		'name' => '[0-9a-f]{32}',
-		'ext'  => '(jpe?g|png|gif)',
+		// 'name' => '[0-9a-f]{32}',
+		'name' => '[^/]+',
+		'ext'  => '((j|J)(p|P)(e|E)?(g|G)|(p|P)(n|N)(g|G)|(g|G)(i|I)(f|F))',
 	)
 )->defaults(array(
 	'controller' => 'resizer',
 	'action'     => 'index',
 ));
-
